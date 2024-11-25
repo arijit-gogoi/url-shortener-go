c3f86ca3ad385e0ae19d53798f5014af6ac94119
Jorghy-Del <91205677+Jorghy-Del@users.noreply.github.com>
2024-11-25 16:55:49 +0530

    feat(db): pass db into handler

--- cmd/web-app/main.go
+++ cmd/web-app/main.go
@@ -1,16 +1,29 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 
 	"github.com/arijit-gogoi/url-shortener-go/internal/controllers"
+	"github.com/arijit-gogoi/url-shortener-go/internal/db"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
+	sqlitedb, err := sql.Open("sqlite3", "db.sqlite")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer sqlitedb.Close()
+
+	if err := db.CreateTable(sqlitedb); err != nil {
+		log.Fatal(err)
+	}
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", controllers.ShowIndex)
-	mux.HandleFunc("/shorten", controllers.Shorten)
+	mux.HandleFunc("/shorten", controllers.Shorten(sqlitedb))
 
 	if err := http.ListenAndServe(":8080", mux); err != nil {
 		log.Fatal(err)
--- internal/controllers/shorten.go
+++ internal/controllers/shorten.go
@@ -1,9 +1,11 @@
 package controllers
 
 import (
+	"database/sql"
 	"html/template"
 	"net/http"
 
+	"github.com/arijit-gogoi/url-shortener-go/internal/db"
 	"github.com/arijit-gogoi/url-shortener-go/internal/url"
 )
 
@@ -16,26 +18,33 @@ func init() {
 	shortenTmplt = template.Must(template.ParseFiles("internal/views/shorten.html"))
 }
 
-func Shorten(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method not allowd", http.StatusMethodNotAllowed)
-		return
+func Shorten(sqlitedb *sql.DB) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method not allowd", http.StatusMethodNotAllowed)
+			return
+		}
+
+		// userURL is the URL that the client wants to shorten.
+		usersURL := r.FormValue("url")
+		if usersURL == "" {
+			http.Error(w, "URL not provided", http.StatusBadRequest)
+			return
+		}
+
+		usersURL = url.Sanitise(usersURL)
+		shortURL := url.Shorten(usersURL)
+
+		err := db.StoreURL(sqlitedb, shortURL, usersURL)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		data = map[string]string{
+			"ShortURL": shortURL,
+		}
+
+		// Execute the shorten template with data.
+		shortenTmplt.Execute(w, data)
 	}
-
-	// userURL is the URL that the client wants to shorten.
-	usersURL := r.FormValue("url")
-	if usersURL == "" {
-		http.Error(w, "URL not provided", http.StatusBadRequest)
-		return
-	}
-
-	usersURL = url.Sanitise(usersURL)
-	shortURL := url.Shorten(usersURL)
-
-	data = map[string]string{
-		"ShortURL": shortURL,
-	}
-
-	// Execute the shorten template with data.
-	shortenTmplt.Execute(w, data)
 }
